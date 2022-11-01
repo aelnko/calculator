ca641e29096fdb86154cd6590c771417dbeed530
Elena <lekolupaeva1201@gmail.com>
2022-11-01 05:04:33 +0200

    add some fucntionality

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+const buttons = document.querySelectorAll('button');
+let result = document.querySelector('.result__value');
+
+const numbers = '1234567890'.split('');
+
+let operand;
+let num1;
+let num2;
+
+buttons.forEach((btn) => {
+  const value = btn.innerHTML;
+  btn.addEventListener('click', () => {
+    if (numbers.includes(value)) {
+      result.innerHTML = result.innerHTML === '0' ? value : result.innerHTML + value;
+      num2 = num1 === undefined ? undefined : result.innerHTML;
+    } else if (value === 'C') {
+      result.innerHTML = '0';
+    } else if (value === '.') {
+      result.innerHTML += value;
+    } else if (value === '=') {
+      switch(operand) {
+        case '+': 
+          result.innerHTML = Number(num1) + Number(num2);
+          break;
+        case '-':
+          result.innerHTML = Number(num1) - Number(num2);
+          break;
+        case '%':
+          result.innerHTML = Number(num1) % Number(num2);
+          break;
+        case '÷':
+          result.innerHTML = Number(num1) / Number(num2);
+          break;
+        case '×':
+          result.innerHTML = Number(num1) * Number(num2);
+          break;
+      }
+    } else {
+      num1 = Number(result.innerHTML);
+      result.innerHTML = '0';
+      operand = value;
+    }
+  })
+})
